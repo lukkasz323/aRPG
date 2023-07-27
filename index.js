af64edf7a2298b6714cee144c2d0afb9f8454396
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-07-27 21:29:20 +0200

    txt fetching debug

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { Game } from "./core/Game.js";
+fetch("./data/test.txt")
+    .then((res) => res.text())
+    .then((text) => {
+    console.log(text);
+});
 const canvas = document.getElementById("game-canvas");
 const game = new Game(canvas);
 game.run();
--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import { Game } from "./core/Game.js";
 
+fetch("./data/test.txt")
+    .then((res) => res.text())
+    .then((text) => {
+        console.log(text);
+    });
+
 const canvas = document.getElementById("game-canvas");
 const game = new Game(<HTMLCanvasElement>canvas);
 
